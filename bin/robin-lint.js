59f6dad9defb2666e3d20f55414758318e3f6d08
benyasin <rgwapkk520>
2018-09-04 18:06:29 +0800

    add tslint to global

--- bin/robin-lint.js
+++ bin/robin-lint.js
@@ -5,20 +5,28 @@ const symbols = require('log-symbols');
 const { spawn } = require('child_process');
 
 program.parse(process.argv);
-
-const lint = spawn('npm',['run','tslint --fix --project .'],{cwd:process.cwd()});
-lint.stdout.on('data',(data)=>{
-    console.log(data.toString('utf8'));
-})
+const cmdArgs = [
+  'prettier',
+  `contract/**/*.ts`,
+  '--write',
+  'tslint',
+  '--fix',
+  '--project',
+  '.'
+];
+const lint = spawn('npm', ['run', cmdArgs], { cwd: process.cwd() });
+lint.stdout.on('data', (data) => {
+  console.log(data.toString('utf8'));
+});
 
 lint.stderr.on('data', (data) => {
-    if(data.indexOf('no-unused-variable is deprecated') !== -1){
-        console.log(symbols.warning,chalk.yellow(data.toString('utf8')));
-    }else{
-        console.log(symbols.error,chalk.red(data.toString('utf8')));
-    }
+  if (data.indexOf('no-unused-variable is deprecated') !== -1) {
+    console.log(symbols.warning, chalk.yellow(data.toString('utf8')));
+  } else {
+    console.log(symbols.error, chalk.red(data.toString('utf8')));
+  }
 });
 
 lint.on('close', (code) => {
-    // console.log(`lint `);
+  // console.log(`lint `);
 });

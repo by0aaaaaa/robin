c3d404de95daeb666e9e6f7c4f373ca9e8426462
benyasin <rgwapkk520>
2018-09-29 01:41:23 +0800

    update notify

--- bin/robin.js
+++ bin/robin.js
@@ -1,31 +1,35 @@
 #!/usr/bin/env node
 const program = require('commander');
 const pkg = require('../package.json');
-const commandList = ['init','build','deploy','lint'];
+const updateNotifier = require('update-notifier');
+
+updateNotifier({pkg}).notify();
+
+const commandList = ['init', 'build', 'deploy', 'lint', 'test'];
 
 program
-    .version(pkg.version,'-v,--version')
-    .usage('<command>')
-    .command('init','init your robin project.');
+  .version(pkg.version, '-v,--version')
+  .usage('<command>')
+  .command('init', 'init your robin project.');
 
 program
-    .command('build','token project.');
+  .command('build', 'token project.');
 
 program
-    .command('deploy','deploy smart contract.');
+  .command('deploy', 'deploy smart contract.');
 
 program
-    .command('lint','lint project.');
+  .command('lint', 'lint project.');
 
 program
-    .command('test','test project.');
+  .command('test', 'test project.');
 
 program.parse(process.argv);
 
-if(!commandList.includes(program.args[0])){
-    return program.help();
+if (!commandList.includes(program.args[0])) {
+  return program.help();
 }
 
-if(program.args.length===0){
-    program.help();
+if (program.args.length === 0) {
+  program.help();
 }
